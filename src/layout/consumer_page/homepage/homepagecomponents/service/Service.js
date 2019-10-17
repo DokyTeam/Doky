3456b023a5470f525820d1ab7f3309626144ff41
Daniela Duque <danduquegar@unal.edu.co>
2019-10-17 17:55:52 -0500

    :sparkles: Guarderia catalogo

--- src/layout/consumer_page/homepage/homepagecomponents/service/Service.js
+++ src/layout/consumer_page/homepage/homepagecomponents/service/Service.js
@@ -3,27 +3,26 @@ import pasear from './images/pasear.jpg';
 import salto from './images/salto.jpg';
 import veterinaria from './images/veterinaria.jpg';
 import guarderia from './images/guarderia.jpg';
-import Card_sombra from "../cards/Card_sombra.js";
-
+import {Cardsombra}  from "../cards/Card_sombra.js";
 class Service extends Component{
-
+   
     render(){
         return(
          <div className="container marketing text-align .text-font padding-card">
 
           <div className="row">
-            <Card_sombra titulo= "Paseos" descripcion="Accede a un catálogo de paseadores para tu mascota." foto = {pasear} consumercontenthandler={this.props.consumercontenthandler}>
+            <Cardsombra titulo= "Paseos" descripcion="Accede a un catálogo de paseadores para tu mascota." foto = {pasear} consumercontenthandler={this.props.consumercontenthandler}>
 
-            </Card_sombra>
-            <Card_sombra titulo= "Veterinaria" descripcion="Encuentra la veterinaria de tu preferencia." foto = {veterinaria} consumercontenthandler={this.props.consumercontenthandler}>
+            </Cardsombra>
+            <Cardsombra titulo= "Veterinaria" descripcion="Encuentra la veterinaria de tu preferencia." foto = {veterinaria} consumercontenthandler={this.props.consumercontenthandler}>
 
-            </Card_sombra>
-            <Card_sombra titulo= "Guardería" descripcion="El mejor cuidado para tu mascota cuando no puedes estar con ella." foto = {guarderia} consumercontenthandler={this.props.consumercontenthandler}>
+            </Cardsombra>
+            <Cardsombra titulo= "Guardería" descripcion="El mejor cuidado para tu mascota cuando no puedes estar con ella." foto = {guarderia} consumercontenthandler={this.props.consumercontenthandler}>
 
-            </Card_sombra>
-            <Card_sombra titulo= "Saltos" descripcion="Las mejores mascotas para tus cruces." foto = {salto} consumercontenthandler={this.props.consumercontenthandler}>
+            </Cardsombra>
+            <Cardsombra titulo= "Saltos" descripcion="Las mejores mascotas para tus cruces." foto = {salto} consumercontenthandler={this.props.consumercontenthandler}>
 
-            </Card_sombra> 
+            </Cardsombra> 
             
           </div>
         </div>
---
+++ src/layout/consumer_page/services/Barra/Barra.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+
+import '../../../global_css/textcolors.css';
+import '../../../global_css/colors.css';
+import '../../../global_css/fonts.css';
+import ciudades from './ciudades.json';
+class Barra extends Component {
+    
+    createoption = () => {
+        let table = []
+        // Outer loop to create parent
+
+       table.push (ciudades.map((data, index) => {
+            return <option  key={index} value={data.nombre}> {data.nombre} </option>
+
+        }
+        ))
+        return table
+    }
+
+    render() {
+        return (
+            <div className="leftcontainer ">
+                
+                <nav className="navbar navbar-expand-lg navbar-light bg-light flex-column">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse flex-column" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto flex-column">
+                            <li className="nav-item dropdown">
+                                <h2 className="nav-link ">
+                                    Servicios        </h2>
+                                <div className="dropdown-divider" onClick={() => this.props.consumercontenthandler('consumer_home_page') }></div>
+                                <div className="dropdown-divider" onClick={() => this.props.consumercontenthandler('consumer_home_page') }></div>
+                                <button className="dropdown-item" onClick={() => this.props.consumercontenthandler('consumer_home_page') }>Guardería</button>
+                                <div className="dropdown-divider" onClick={() => this.props.consumercontenthandler('consumer_home_page') } ></div>
+                                <button className="dropdown-item" onClick={() => this.props.consumercontenthandler('consumer_home_page') }>Veterinaria</button>
+                                <div className="dropdown-divider"></div>
+                                <button className="dropdown-item" onClick={() => this.props.consumercontenthandler('consumer_home_page') }>Paseos</button>
+                                <div className="dropdown-divider"></div>
+                                <button className="dropdown-item" onClick={() => this.props.consumercontenthandler('consumer_home_page') } >Saltos</button>
+
+
+                            </li>
+
+                            <li className="nav-item">
+                                <h2 className="nav-link ">Filtros</h2>
+                                <div className="dropdown-divider"></div>
+                                <div className="dropdown-divider"></div>
+                            </li>
+                            <li className="nav-item">
+                                <h5 className="nav-link " >Ubicación</h5>
+                                <div className="input-group">
+                                    <select className="custom-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                        <option defaultValue>Selecione...</option>
+                                        {this.createoption()}
+
+                                    </select>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button">Filtrar</button>
+                                    </div>
+                                </div>
+                                <div className="dropdown-divider"></div>
+                            </li>
+                            <li className="nav-item">
+                                <h5 className="nav-link " >Precio</h5>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">$</span>
+                                        <span className="input-group-text">Desde</span>
+                                    </div>
+                                    <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+                                </div>
+                                <div className="input-group mb-3">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">$</span>
+                                        <span className="input-group-text">Hasta</span>
+                                    </div>
+                                    <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+                                </div>
+                                <div className="input-group-append float-right">
+                                    <button className="btn btn-outline-secondary" type="button">Filtrar</button>
+                                </div>
+                                <div className="dropdown-divider"></div>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+                
+
+            </div>
+        );
+    }
+    
+
+
+}
+
+export { Barra };
