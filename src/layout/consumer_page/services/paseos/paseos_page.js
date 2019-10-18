e31bef956364d25e682309e76436bff3403b7730
Daniela Duque <danduquegar@unal.edu.co>
2019-10-17 21:37:15 -0500

    :sparkles: Catalogo Paseos

--- src/layout/consumer_page/services/paseos/paseos_page.js
+++ src/layout/consumer_page/services/paseos/paseos_page.js
@@ -3,14 +3,36 @@ import React, { Component } from 'react';
 import '../../../global_css/textcolors.css';
 import '../../../global_css/colors.css';
 import '../../../global_css/fonts.css';
-
+import { Barra } from '../Barra/Barra.js';
+import paseos_json from './paseos.json';
+import { ServiciosContenedor } from '../servicios_contenedor/ServiciosConedor';
 class PaseosPage extends Component {
 
     render() {
         return (
             <div className="WhiteColor">
-                <h1>Paseos</h1>
-                <button onClick={() => this.props.consumercontenthandler('consumer_home_page')}>home</button>
+                
+                    <div className="container-fluid">
+                        <div className="row align-items-center">
+                        <div className="row align-items-top">
+                        <div className="col-md-12 col-lg-3 " >
+                            <Barra consumercontenthandler={this.props.consumercontenthandler} />
+                        </div>
+                        <div className="col-md-12 col-lg-9 "  >
+                            <h1 className="CurvyTextFontBig"style={{margin:"5%", textAlign: "center"}}>
+                                Paseos
+                            </h1>
+                             <ServiciosContenedor json={paseos_json}></ServiciosContenedor>
+                            
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+                
+
+
             </div>
         );
     }
--- src/layout/consumer_page/services/veterinaria/veterinaria_page.js
+++ src/layout/consumer_page/services/veterinaria/veterinaria_page.js
@@ -3,14 +3,30 @@ import React, { Component } from 'react';
 import '../../../global_css/textcolors.css';
 import '../../../global_css/colors.css';
 import '../../../global_css/fonts.css';
+import { Barra } from '../Barra/Barra.js';
 
 class VeterinariaPage extends Component {
 
     render() {
         return (
             <div className="WhiteColor">
-                <h1>Veterinaria</h1>
-                <button onClick={() => this.props.consumercontenthandler('consumer_home_page')}>home</button>
+                
+                    <div className="container-fluid">
+                        <div className="row align-items-center">
+                            <div className="col-md-12 col-lg-3">
+                                <Barra />
+                            </div>
+                            <div className="col-md-12 col-lg-9">
+                                guarderia
+                        <button onClick={() => this.props.consumercontenthandler('consumer_home_page')}>home</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                
+
+
             </div>
         );
     }
