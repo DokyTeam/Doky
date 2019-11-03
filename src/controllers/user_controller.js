f47eb26f3efbec4d847318d87885e8c55cced112
Vale <vbernalb@unal.edu.co>
2019-11-02 20:56:36 -0500

    Actualizar puntuacion
    Se agrega el repositorio de actualización + función de actualizar puntuación (updateStars) en user_controller

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,9 +1,7 @@
-
-
-
 import fire from '../config/Fire';
 import {FirebaseReadRepository} from '../access_data/firebase_read_repository';
 import {FirebaseCreateRepository} from '../access_data/firebase_create_repository';
+import {FirebaseUpdateRepository} from '../access_data/firebase_update_repository';
 
 
 
@@ -13,15 +11,28 @@ export class UserController {
         this.firebaseInstance = fire;
         this.firebaseReadRepository = new FirebaseReadRepository();
         this.firebaseCreateRepository = new FirebaseCreateRepository();
+        this.firebaseUpdateRepository = new FirebaseUpdateRepository();
     }
 
     //es  necesario que se pase un objeto en lugar del tipo especifico
     createUser(email, tipo) {
-        let m = {
-            tipo : tipo
-        };
+        let m;
 
-        return this.firebaseCreateRepository.writeCollectionIdDefined('usuarios',email,m);
+        
+        if (tipo == "Prestador") {
+            m = {
+                tipo : tipo,
+                sumapuntuacion : 0,
+                cantidadpuntuacion : 0
+            };  
+        } else {
+            m = {
+                tipo : tipo
+            };
+        }
+
+
+           return this.firebaseCreateRepository.writeCollectionIdDefined('usuarios',email,m);
     }
 
     async getTipoUsuario(email) {
@@ -44,20 +55,26 @@ export class UserController {
     }
     
     
-addMascota(email,mascotaInfo){
+    addMascota(email,mascotaInfo){
 
-    return this.firebaseCreateRepository.writeCollectionIdDefined('usuarios/'+ email +"/mascotas/",mascotaInfo.nombre,mascotaInfo);
-    
+        return this.firebaseCreateRepository.writeCollectionIdDefined('usuarios/'+ email +"/mascotas/",mascotaInfo.nombre,mascotaInfo);
+        
+    }
+
+
+
+    //recibe el email del usuario y el nuevo valor
+    updateStars(email, nuevaPuntuacion){
+        return this.firebaseReadRepository.readCollection("usuarios").doc(email).get().then(
+            querySnapshot => {
+                let sp =querySnapshot.data().sumapuntuacion + nuevaPuntuacion;
+                let cp =  querySnapshot.data().cantidadpuntuacion + 1;
+                let m = {
+                    sumapuntuacion : sp,
+                    cantidadpuntuacion : cp
+                }
+                return this.firebaseUpdateRepository.updateAttributesDocument("usuarios",email,m);
+            })
+    }
+
 }
-
-
-}
-
-
-
-
-
-
-
-
-
